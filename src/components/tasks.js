486f8c3cdfa892199e264da27882aa42b3f4aba1
Aqeel <aqeel-sheikh@users.noreply.github.com>
2025-08-13 21:44:48 +0530

    feat: Implemented localStorage functionality

--- src/components/tasks.js
+++ src/components/tasks.js
@@ -1,52 +1,68 @@
+import dotsImg from "../assets/icons/three-dots.svg";
+import redCircle from "../assets/icons/red-circle.svg";
+import blueCircle from "../assets/icons/blue-circle.svg";
+import greenCircle from "../assets/icons/green-circle.svg";
+
 export class task {
-  constructor(title, dueDate, priority, description,) {
-    (this.title = title), (this.description = description);
-    (this.dueDate = dueDate), (this.priority = priority);
+  constructor(title, dueDate, priority, description) {
+    (this.title = title),
+      (this.dueDate = dueDate),
+      (this.priority = priority),
+      (this.description = description),
+      (this.dateCreated = generateDate()),
+      (this.id = crypto.randomUUID());
   }
 }
+function generateDate() {
+  const today = new Date();
+  const day = String(today.getDate()).padStart(2, "0");
+  const month = String(today.getMonth() + 1).padStart(2, "0");
+  const year = today.getFullYear();
+
+  return `${day}-${month}-${year}`;
+}
+
 const myDialog = document.getElementById("myDialog");
-const form = document.querySelector("#taskForm")
-form.addEventListener("submit", (e)=>{
-    e.preventDefault()
-})
+const form = document.querySelector("#taskForm");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
+
+const tasksList = JSON.parse(localStorage.getItem("myData")) || [];
+
 export function addTask() {
-    document.querySelector("#done").addEventListener("click", () => {
-      const titleELement = document.querySelector("#title")
-    const title = titleELement.value
-    let date = document.querySelector("#date").value;
-    let priority = document.querySelector('input[name="priority"]:checked').value;
-    let description = document.querySelector("#description").value;
+  document.querySelector("#done").addEventListener("click", () => {
+    const titleELement = document.querySelector("#title");
+    const title = titleELement.value;
+    
+    const date = document.querySelector("#date").value;
+    
+    const priority = document.querySelector(
+      'input[name="priority"]:checked'
+    ).value;
+    
+    const description = document.querySelector("#description").value;
+    
+    const t = new task(title, date, priority, description);
+    showTask(title, date, priority, description);
 
-    showTask(title,date,priority,description)
-    const t = new task(title,date, priority, description);
-    myDialog.close()
-    form.reset()
+    tasksList.push(t);
+    localStorage.setItem("myData", JSON.stringify(tasksList));
+    
+    // const savedData = JSON.parse(localStorage.getItem("myData")) || [];
 
+    myDialog.close();
+    form.reset();
   });
 }
-export function showTask(t,da,p,d){
-    const div = document.createElement("div")
-    const titleP = document.createElement("p")
-    titleP.textContent = t
-    const dateP = document.createElement("p")
-    dateP.textContent = da
-    const priorityP = document.createElement("p")
-    priorityP.textContent = p
-    const descriptionP = document.createElement("p")
-    descriptionP.textContent = d
 
-    div.append(titleP,dateP, priorityP, descriptionP )
-
-    document.querySelector("#content").appendChild(div)
-}
 export function showAddTaskForm() {
-  
   document.querySelector("#addTask").addEventListener("click", () => {
     myDialog.showModal();
   });
   document.querySelector("#closeDialog").addEventListener("click", () => {
     myDialog.close();
-    form.reset()
+    form.reset();
   });
 
   myDialog.addEventListener("click", (e) => {
@@ -61,4 +77,109 @@ export function showAddTaskForm() {
       myDialog.close();
     }
   });
+}
+
+export function showTask(title, dueDate, priority, description) {
+  const task = document.createElement("div");
+  task.classList.add("task");
+
+  const menuContainer = document.createElement("div");
+  menuContainer.classList.add("menu-container");
+
+  const dotsBtn = document.createElement("img");
+  dotsBtn.src = dotsImg;
+  dotsBtn.classList.add("dots");
+
+  const menu = document.createElement("div");
+  menu.classList.add("menu", "hide");
+
+  const editBtn = document.createElement("a");
+  editBtn.textContent = "Edit";
+  const dltBtn = document.createElement("a");
+  dltBtn.textContent = "Delete";
+
+  menu.append(editBtn, dltBtn);
+  menuContainer.append(dotsBtn, menu);
+
+  const taskDetails = document.createElement("div");
+  taskDetails.classList.add("task-details");
+  const priorityImg = document.createElement("img");
+  priorityImg.classList.add("circle");
+
+  const taskTitle = document.createElement("h4");
+  taskTitle.textContent = title;
+
+  const taskDescription = document.createElement("p");
+  taskDescription.textContent = description;
+
+  const aboutTask = document.createElement("div");
+  aboutTask.classList.add("about-task");
+
+  const taskPriority = document.createElement("p");
+  const pSpan = document.createElement("span");
+  pSpan.classList.add("priority");
+  pSpan.textContent = priority;
+  taskPriority.textContent = "Priority: ";
+  taskPriority.appendChild(pSpan);
+
+  const taskDueDate = document.createElement("p");
+  const dSpan = document.createElement("span");
+  dSpan.classList.add("date");
+  dSpan.textContent = dueDate;
+  taskDueDate.textContent = "Due Date: ";
+  taskDueDate.appendChild(dSpan);
+
+  if (priority === "Extreme") {
+    priorityImg.src = redCircle;
+    task.style.backgroundColor = "#e645450e";
+    pSpan.style.color = "#ff0000";
+  } else if (priority === "Moderate") {
+    priorityImg.src = blueCircle;
+    task.style.backgroundColor = "#1d32ce0e";
+    pSpan.style.color = "#0000ff";
+  } else {
+    priorityImg.src = greenCircle;
+    task.style.backgroundColor = "#00ff001c";
+    pSpan.style.color = "#00ff00";
+  }
+
+  taskDetails.append(priorityImg, taskTitle, taskDescription);
+  aboutTask.append(taskPriority, taskDueDate);
+
+  task.append(menuContainer, taskDetails, aboutTask);
+
+  document.querySelector(".tasks").appendChild(task);
+
+  dotsBtn.addEventListener("click", () => {
+    menu.classList.toggle("hide");
+    if (!dotsBtn.style.position) {
+      dotsBtn.style.position = "absolute";
+    } else {
+      dotsBtn.style.position = "";
+    }
+  });
+}
+
+{
+  /* <div class="task">
+  <div class="task-details">
+    <img class="circle" src="./assets/icons/blue-circle.svg" alt="" />
+
+    <h4>Task 1</h4>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, itaque.
+    </p>
+  </div>
+  <div class="about-task">
+    <p>
+      Priority: <span class="priority">Moderate</span>
+    </p>
+    <p>
+      Status: <span class="status">In progress</span>
+    </p>
+    <p>
+      Due date: <span class="date">01-01-2025</span>
+    </p>
+  </div>
+</div>; */
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,42 @@
-import "./css/style.css"
-import "./css/responsive.css"
+import "./css/style.css";
+import "./css/responsive.css";
 
-import {task, showAddTaskForm, addTask} from "./components/tasks.js"
-import { hideSidebarOnSmallScreens, handleNavEvents } from "./components/responsive.js"
+import {
+  task,
+  showAddTaskForm,
+  addTask,
+  showTask,
+  tasksList,
+} from "./components/tasks.js";
+import {
+  hideSidebarOnSmallScreens,
+  handleNavEvents,
+} from "./components/responsive.js";
 
-const task1 = new task("Make todo list", "it is project from TOD", "11/08/2025", "High")
+const task1 = new task(
+  "Make todo list",
+  "it is project from TOD",
+  "11/08/2025",
+  "High"
+);
 
-showAddTaskForm()
-addTask()
-hideSidebarOnSmallScreens()
-document.querySelector('.hamburger').addEventListener('click', handleNavEvents().openNav);
-document.querySelector('.navCloseBtn').addEventListener('click', handleNavEvents().closeNav);
+showAddTaskForm();
+addTask();
+hideSidebarOnSmallScreens();
+document
+  .querySelector(".hamburger")
+  .addEventListener("click", handleNavEvents().openNav);
+document
+  .querySelector(".navCloseBtn")
+  .addEventListener("click", handleNavEvents().closeNav);
 
+document.addEventListener("DOMContentLoaded", () => {
+  const savedData = JSON.parse(localStorage.getItem("myData")) || [];
+  for (let task of savedData) {
+    showTask(task.title, task.date, task.priority, task.description);
+  }
+});
+
+function showCompletedTasks (arr) {
+    const completedTasksContainer = document.querySelector(".completed-tasks")
+}
